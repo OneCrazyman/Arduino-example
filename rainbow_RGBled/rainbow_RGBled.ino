f0065aef461d6c3e09a48622c575bc4b7b8619b2
cxxae1 <cjw7155@naver.com>
2022-01-26 13:04:11 +0900

    3색 LED 사용 예제

---
+++ rainbow_RGBled/rainbow_RGBled.ino
@@ -0,0 +1,39 @@
+//RGB LED이용한 빨주노초파남보(무지개색) 점등
+//해당핀 지정
+int pinR = A0;
+int pinG = A1;
+int pinB = A2;
+
+//7가지 색을 배열로 구현
+int RGB[][3]={
+  {255,0,0},
+  {255,130,0},
+  {255,220,0},
+  {0,255,0},
+  {0,0,255},
+  {100,0,200},
+  {200,0,255}
+};
+
+
+//아웃풋 지정
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinR,OUTPUT);
+  pinMode(pinG,OUTPUT);
+  pinMode(pinB,OUTPUT);
+  //콘솔 명령을 보기위해 설정
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //7가지 색을 하나씩 반복하여 입력
+  for (int i=0; i<7; i++){
+    analogWrite(pinR,RGB[i][0]);
+    analogWrite(pinG,RGB[i][1]);
+    analogWrite(pinB,RGB[i][2]);
+    
+    delay(1000);
+  }
+}
